759cbeae8509054be200998be4a8cbc9957fe2d4
Karnag <albertolopma@gmail.com>
2023-12-24 02:58:51 +0100

    terminado Serpiente

--- JavaScript/Serpiente/game.js
+++ JavaScript/Serpiente/game.js
@@ -1,12 +1,13 @@
 //Elementos de HTML
 const tablero = document.getElementById("tableroJuego");
 const puntuacion = document.getElementById("puntuacion");
+const puntuacionMax = document.getElementById("puntuacionMax");
 const botonStart = document.getElementById("start");
 const imagenGameOver = document.getElementById("gameOver");
-
+const contenedorTablero= document.getElementById("contenedor")
 //Settings del Juego
 const sizeTablero = 10;
-const celocidadJuego = 100;
+var velocidadJuego = 300;
 const tiposCuadrado = {
   vacio: 0,
   serpiente: 1,
@@ -14,20 +15,19 @@ const tiposCuadrado = {
 };
 
 const direcciones = {
-  arriba: -10,
-  abajo: 10,
-  derecha: 1,
-  izquierda: -1,
+  ArrowUp: -10,
+  ArrowDown: 10,
+  ArrowRight: 1,
+  ArrowLeft: -1,
 };
 //Variables del juego
 var snake;
 var score;
+var scoreMax='';
 var direction;
 var cuadroTablero;
 var cuadroVacio;
 var intervaloMovimiento;
-
-
 
 const crearComidaAleatoria = () => {
   const lugarVacioRandom =
@@ -36,6 +36,11 @@ const crearComidaAleatoria = () => {
 };
 const actualizarScore = () => {
   puntuacion.innerHTML = score;
+  puntuacionMax.innerHTML = scoreMax;
+  if(score>scoreMax){
+    scoreMax=score;
+    puntuacionMax.innerHTML = scoreMax;
+  }
 };
 const colocarSerpiente = () => {
   snake.forEach((cuadrado) => dibujarCuadrado(cuadrado, "serpiente"));
@@ -58,19 +63,71 @@ const dibujarCuadrado = (cuadrado, tipo) => {
   }
 };
 
-const eventoDireccion = key => {
+const movimientoSerpiente = () => {
+  const nuevoCuadrado = String(
+    Number(snake[snake.length - 1]) + direcciones[direction]
+  ).padStart(2, "0");
+  const [row, colum] = nuevoCuadrado.split("");
+
+  if (
+    nuevoCuadrado < 0 ||
+    nuevoCuadrado > sizeTablero * sizeTablero ||
+    (direction === "ArrowRight" && colum == 0) ||
+    (direction === "ArrowLeft" && colum == 9) ||
+    cuadroTablero[row][colum] === tiposCuadrado.serpiente
+  ) {
+    gameOver();
+  } else {
+    snake.push(nuevoCuadrado);
+    if (cuadroTablero[row][colum] === tiposCuadrado.comida) {
+      cojerComida();
+    } else {
+      const cuadroVacio = snake.shift();
+      dibujarCuadrado(cuadroVacio, "vacio");
+    }
+    colocarSerpiente();
+  }
+};
+const cojerComida = () => {
+  score++;
+  velocidadJuego -=5;
+  clearInterval(intervaloMovimiento);
+  intervaloMovimiento = setInterval(
+    () => movimientoSerpiente(),
+    velocidadJuego
+  );
+  console.log(velocidadJuego);
+  actualizarScore();
+  crearComidaAleatoria();
+};
+
+const gameOver = () => {
+  imagenGameOver.style.display = "block";
+  clearInterval(intervaloMovimiento);
+  velocidadJuego=300;
+  botonStart.style.display = "block";
+};
+
+const setDireccion = (newDirection) => {
+  direction = newDirection;
+};
+
+const eventoDireccion = (key) => {
+  console.log(key.code);
   switch (key.code) {
-    case "arriba":
-      direction != "abajo" && setDirection(key.code);
+     case "ArrowDown":
+      direction != "ArrowUp" && setDireccion(key.code);
       break;
-    case "abajo":
-      direction != "arriba" && setDirection(key.code);
+      
+      case "ArrowUp":
+      direction != "ArrowDown" && setDireccion(key.code);
       break;
-    case "derecha":
-      direction != "izquierda" && setDirection(key.code);
+   
+    case "ArrowRight":
+      direction != "ArrowLeft" && setDireccion(key.code);
       break;
-    case "izquierda":
-      direction != "derecha" && setDirection(key.code);
+    case "ArrowLeft":
+      direction != "ArrowRight" && setDireccion(key.code);
       break;
   }
 };
@@ -89,9 +146,9 @@ const crearTablero = () => {
 };
 
 const setGame = () => {
-  snake = ["00", "01", "02"];
-  score = snake.length;
-  direction = "derecha";
+  snake = ["10", "11", "12"];
+  score = snake.length;  
+  direction = "ArrowRight";
   cuadroTablero = Array.from(Array(sizeTablero), () =>
     new Array(sizeTablero).fill(tiposCuadrado.vacio)
   );
@@ -104,10 +161,15 @@ const startGame = () => {
   setGame();
   imagenGameOver.style.display = "none";
   botonStart.style.display = "none";
+  contenedorTablero.style.display="block"
   colocarSerpiente();
   actualizarScore();
   crearComidaAleatoria();
   document.addEventListener("keydown", eventoDireccion);
+  intervaloMovimiento = setInterval(
+    () => movimientoSerpiente(),
+    velocidadJuego
+  );
 };
 
 botonStart.addEventListener("click", startGame);
