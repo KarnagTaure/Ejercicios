5087e575dde1a11b258f74cad5ce1aff0a617e49
Karnag <albertolopma@gmail.com>
2023-12-30 00:10:34 +0100

    ejercicio1

---
+++ JavaScript/Node/Ejercicio1/src/index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
+const app = express();
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+console.log(join(__dirname, "views"));
+
+app.set("views", join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => res.render("index"));
+
+app.get("/about", (req, res) => res.render("about"));
+
+app.get("/contact", (req, res) => res.render("contact"));
+
+
+app.listen(3000);
+console.log("server listenig ", 3000);
--- JavaScript/Node/src/index.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-
-const app = express()
-
-app.get('/', (res,res)=> res.send('hello mundo'))
-
-app.listen(3000)
-console.log("server listenig ", 3000)
